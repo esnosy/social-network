3fc59c541198c8ce446917a5160eb349e69e24c2
Eyad Senosy <esnosy@proton.me>
2026-05-03 17:47:50 +0300

    Use supabase instead of mongodb

--- app.py
+++ app.py
@@ -5,31 +5,23 @@ import secrets
 import datetime
 from math import ceil
 
+from supabase import create_client, Client
 from dotenv import load_dotenv
-from flask import (
-    Flask,
-    flash,
-    redirect,
-    render_template,
-    request,
-    session,
-    abort
-)
-from pymongo import MongoClient
-from flask_session import Session
+from flask import Flask, flash, redirect, render_template, request, session, abort
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-load_dotenv(".env.local")
-
-mongo = MongoClient(os.environ["MONGODB_URI"])
+load_dotenv(".env")
 
 app = Flask(__name__)
-app.config["SESSION_TYPE"] = "mongodb"
-app.config["SESSION_MONGODB"] = mongo
-Session(app)
+app.config["SECRET_KEY"] = os.environ.get("FLASK_SECRET_KEY", "")
+
+supabase: Client = create_client(
+    os.environ.get("SUPABASE_URL", ""),
+    os.environ.get("SUPABASE_KEY", ""),
+)
 
 
 @app.get("/profile/<username>")
@@ -37,33 +29,41 @@ def profile(username: str):
     signed_in_username = session.get("username")
     if signed_in_username is None:
         return redirect("/signin")
-    user = mongo.users.users.find_one({"name": username})
-    if user is None:
+    res = supabase.table("users").select("*").eq("name", username).execute()
+    if len(res.data) == 0:
         return abort(404)
 
     page = request.args.get("page", 1, type=int)
     page_size = 10
 
-    posts_agg = list(mongo.posts.posts.aggregate(
-        [
-            {"$match": {"username": username}},
-            # Sort from newest to oldest
-            {"$sort": {"timestamp": -1}},
-            {
-                "$facet": {
-                    "metadata": [{"$count": "totalCount"}],
-                    "data": [{"$skip": (page - 1) * page_size}, {"$limit": page_size}],
-                },
-            },
-        ]
-    ))
-    posts = posts_agg[0]["data"]
-    total_count = posts_agg[0]["metadata"][0]["totalCount"]
+    start = (page - 1) * page_size
+    end = start + page_size - 1  # Supabase range is inclusive
+
+    res = (
+        supabase.table("users").select("*, posts(count)").eq("name", username).execute()
+    )
+    total_count = res.data[0]["posts"][0]["count"]
+    if total_count == 0:
+        posts = []
+    else:
+        res = (
+            supabase.table("posts")
+            .select("*")
+            .eq("user_id", session["user_id"])
+            .order("created_at", desc=True)
+            .range(start, end)
+            .execute()
+        )
+        posts = res.data
     total_pages = ceil(total_count / page_size)
-    is_last = page == total_pages
+    is_last = page == total_pages or total_count == 0
     is_first = page == 1
     return render_template(
-        "profile.html", username=username, posts=posts, is_last=is_last, is_first=is_first
+        "profile.html",
+        username=username,
+        posts=posts,
+        is_last=is_last,
+        is_first=is_first,
     )
 
 
@@ -73,14 +73,9 @@ def post():
     if username is None:
         return redirect("/signin")
     text = request.form.get("post")
-    collection = mongo.posts.posts
-    collection.insert_one(
-        {
-            "username": username,
-            "text": text,
-            "timestamp": datetime.datetime.now(tz=datetime.timezone.utc),
-        }
-    )
+    supabase.table("posts").insert(
+        {"user_id": session["user_id"], "text": text}
+    ).execute()
     return redirect("/")
 
 
@@ -93,23 +88,26 @@ def index():
     page = request.args.get("page", 1, type=int)
     page_size = 10
 
-    # https://codebeyondlimits.com/articles/pagination-in-mongodb-the-only-right-way-to-implement-it-and-avoid-common-mistakes
-    posts_agg = list(mongo.posts.posts.aggregate(
-        [
-            # Sort from newest to oldest
-            {"$sort": {"timestamp": -1}},
-            {
-                "$facet": {
-                    "metadata": [{"$count": "totalCount"}],
-                    "data": [{"$skip": (page - 1) * page_size}, {"$limit": page_size}],
-                },
-            },
-        ]
-    ))
-    posts = posts_agg[0]["data"]
-    total_count = posts_agg[0]["metadata"][0]["totalCount"]
+    start = (page - 1) * page_size
+    end = start + page_size - 1  # Supabase range is inclusive
+
+    res = supabase.table("posts").select("*", count="exact", head=True).execute()
+    total_count = res.count
+
+    if total_count == 0:
+        posts = []
+    else:
+        res = (
+            supabase.table("posts")
+            .select("*, users(name)")
+            .order("created_at", desc=True)
+            .range(start, end)
+            .execute()
+        )
+        posts = res.data
+
     total_pages = ceil(total_count / page_size)
-    is_last = page == total_pages
+    is_last = page == total_pages or total_count == 0
     is_first = page == 1
     return render_template(
         "index.html", username=username, posts=posts, is_last=is_last, is_first=is_first
@@ -128,9 +126,8 @@ def signup():
             flash("Please provide username")
             return render_template("signup.html", username=username, password=password)
 
-        db = mongo.users
-        c = db.users.count_documents({"name": username})
-        if c > 0:
+        res = supabase.table("users").select("*").eq("name", username).execute()
+        if len(res.data) > 0:
             flash("User already exists, choose a different user name")
             return render_template("signup.html", username=username, password=password)
 
@@ -140,12 +137,14 @@ def signup():
 
         salt = secrets.token_hex(nbytes=16)
         salted_password = password + salt
-        password_hash = hashlib.sha256(
-            salted_password.encode("utf-8")).hexdigest()
-        db.users.insert_one(
-            {"name": username, "password_hash": password_hash, "salt": salt}
+        password_hash = hashlib.sha256(salted_password.encode("utf-8")).hexdigest()
+        res = (
+            supabase.table("users")
+            .insert({"name": username, "password_hash": password_hash, "salt": salt})
+            .execute()
         )
         session["username"] = username
+        session["user_id"] = res.data[0]["id"]
         return redirect("/")
     return render_template("signup.html")
 
@@ -158,25 +157,24 @@ def signin():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        if not username or len(username) < 8:
-            flash("Please provide username of at least length 8")
+        if not username:
+            flash("Please provide username")
             return render_template("signin.html", username=username, password=password)
 
         if not password or len(password) < 8:
             flash("Please provide password of at least length 8")
             return render_template("signin.html", username=username, password=password)
-        db = mongo.users
-        user = db.users.find_one({"name": username})
-        if user is None:
+        res = supabase.table("users").select("*").eq("name", username).execute()
+        if len(res.data) == 0:
             flash("User not found")
             return render_template("signin.html", username=username, password=password)
-        salt = user["salt"]
-        db_password_hash = user["password_hash"]
-        salted_password = password + salt
-        password_hash = hashlib.sha256(
-            salted_password.encode("utf-8")).hexdigest()
+        db_salt = res.data[0]["salt"]
+        db_password_hash = res.data[0]["password_hash"]
+        salted_password = password + db_salt
+        password_hash = hashlib.sha256(salted_password.encode("utf-8")).hexdigest()
         if db_password_hash == password_hash:
             session["username"] = username
+            session["user_id"] = res.data[0]["id"]
             return redirect("/")
         else:
             flash("Wrong password")
@@ -189,3 +187,7 @@ def signin():
 def signout():
     session.clear()
     return redirect("/signin")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
